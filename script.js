9ac2365ec933ed0b9ff6d36223887aa7339789e5
Oleksandr <lysenkoa86@gmail.com>
2018-06-07 14:08:01 +0300

    little fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function(){
   setInterval(function(){
     const time = new Date();
     const hours = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
-    const minutes = time.getMinutes() < 10 ? '0' + time.Minutes() : time.getMinutes();
+    const minutes = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
     const seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
     $('.currnet-time span').text(hours + ':' + minutes + ':' + seconds);
     if (counterOn){
